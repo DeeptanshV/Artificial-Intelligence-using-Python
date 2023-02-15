f43516f39e851db97eb6767d34d89080dc181561
Deeptansh Verma <75324676+DeeptanshV@users.noreply.github.com>
2023-02-15 14:59:34 +0530

    Update Constrain Satisfactory Method.py

--- Constrain Satisfactory Method.py
+++ Constrain Satisfactory Method.py
@@ -1,56 +1,140 @@
-from queue import PriorityQueue
-v = 14
-graph = [[] for i in range(v)]
+# Python3 program for the above approach
 
-# Function For Implementing Best First Search
-# Gives output path having lowest cost
-
-
-def best_first_search(actual_Src, target, n):
-	visited = [False] * n
-	pq = PriorityQueue()
-	pq.put((0, actual_Src))
-	visited[actual_Src] = True
+# Function to check if the
+# assignment of digits to
+# characters is possible
+def isSolvable(words, result):
+	# Stores the value
+	# assigned to alphabets
+	mp = [-1]*(26)
 	
-	while pq.empty() == False:
-		u = pq.get()[1]
-		# Displaying the path having lowest cost
-		print(u, end=" ")
-		if u == target:
-			break
+	# Stores if a number
+	# is assigned to any
+	# character or not
+	used = [0]*(10)
 
-		for v, c in graph[u]:
-			if visited[v] == False:
-				visited[v] = True
-				pq.put((c, v))
-	print()
+	# Stores the sum of position
+	# value of a character
+	# in every string
+	Hash = [0]*(26)
 
-# Function for adding edges to graph
+	# Stores if a character
+	# is at index 0 of any
+	# string
+	CharAtfront = [0]*(26)
 
+	# Stores the string formed
+	# by concatenating every
+	# occurred character only
+	# once
+	uniq = ""
 
-def addedge(x, y, cost):
-	graph[x].append((y, cost))
-	graph[y].append((x, cost))
+	# Iterator over the array,
+	# words
+	for word in range(len(words)):
+		# Iterate over the string,
+		# word
+		for i in range(len(words[word])):
+			# Stores the character
+			# at ith position
+			ch = words[word][i]
 
+			# Update Hash[ch-'A]
+			Hash[ord(ch) - ord('A')] += pow(10, len(words[word]) - i - 1)
 
-# The nodes shown in above example(by alphabets) are
-# implemented using integers addedge(x,y,cost);
-addedge(0, 2, 3)
-addedge(0, 1, 6)
-addedge(0, 4, 5)
-addedge(1, 3, 9)
-addedge(1, 5, 8)
-addedge(2, 6, 12)
-addedge(2, 7, 14)
-addedge(3, 8, 7)
-addedge(7, 9, 5)
-addedge(8, 10, 6)
-addedge(9, 11, 1)
-addedge(9, 12, 10)
-addedge(9, 13, 2)
+			# If mp[ch-'A'] is -1
+			if mp[ord(ch) - ord('A')] == -1:
+				mp[ord(ch) - ord('A')] = 0
+				uniq += str(ch)
 
-source = int(input("Enter the Source Node: "))
-target = int(input("Enter the Target/Destination Node: "))
-best_first_search(source, target, v)
+			# If i is 0 and word
+			# length is greater
+			# than 1
+			if i == 0 and len(words[word]) > 1:
+				CharAtfront[ord(ch) - ord('A')] = 1
 
-# This code is contributed by Jyotheeswar Ganne
+	# Iterate over the string result
+	for i in range(len(result)):
+		ch = result[i]
+
+		Hash[ord(ch) - ord('A')] -= pow(10, len(result) - i - 1)
+
+		# If mp[ch-'A] is -1
+		if mp[ord(ch) - ord('A')] == -1:
+			mp[ord(ch) - ord('A')] = 0
+			uniq += str(ch)
+
+		# If i is 0 and length of
+		# result is greater than 1
+		if i == 0 and len(result) > 1:
+			CharAtfront[ord(ch) - ord('A')] = 1
+
+	mp = [-1]*(26)
+
+	# Recursive call of the function
+	return True
+
+# Auxiliary Recursive function
+# to perform backtracking
+def solve(words, i, S, mp, used, Hash, CharAtfront):
+	# If i is word.length
+	if i == len(words):
+		# Return true if S is 0
+		return S == 0
+
+	# Stores the character at
+	# index i
+	ch = words[i]
+
+	# Stores the mapped value
+	# of ch
+	val = mp[ord(words[i]) - ord('A')]
+
+	# If val is -1
+	if val != -1:
+		# Recursion
+		return solve(words, i + 1, S + val * Hash[ord(ch) - ord('A')], mp, used, Hash, CharAtfront)
+
+	# Stores if there is any
+	# possible solution
+	x = False
+
+	# Iterate over the range
+	for l in range(10):
+		# If CharAtfront[ch-'A']
+		# is true and l is 0
+		if CharAtfront[ord(ch) - ord('A')] == 1 and l == 0:
+			continue
+
+		# If used[l] is true
+		if used[l] == 1:
+			continue
+
+		# Assign l to ch
+		mp[ord(ch) - ord('A')] = l
+
+		# Marked l as used
+		used[l] = 1
+
+		# Recursive function call
+		x |= solve(words, i + 1, S + l * Hash[ord(ch) - ord('A')], mp, used, Hash, CharAtfront)
+
+		# Backtrack
+		mp[ord(ch) - ord('A')] = -1
+
+		# Unset used[l]
+		used[l] = 0
+
+	# Return the value of x;
+	return x
+
+arr = ["SEND","MORE","MONEY" ]
+S = "TWENTY"
+
+# Function Call
+if isSolvable(arr, S):
+	print("Yes")
+else:
+	print("No")
+	
+	# This code is contributed by mukesh07.
